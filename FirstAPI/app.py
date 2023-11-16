33ad0755265cdf58419f95de18e5c1c07c2a6f37
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-17 00:29:27 +0530

    FastAPI

--- FirstAPI/app.py
+++ FirstAPI/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 # Define what the app does
 @app.get("/")
 def index():
-    name  = request.args.get("name")
-    response = {"data":f"Hello, {name}"}
+    fname  = request.args.get("fname")
+    lname  = request.args.get("lname")
+    fullname = request.args.get("fullname")
+    #handeling the errors
+    if not (fname or lname or fullname):
+        error_message = "pleae try other keyword it's and error"
+        error = {"status":error_message}
+        return jsonify(error)
+    response = {"data":f"Hello, {fname or  lname or fullname}"}
     return response
